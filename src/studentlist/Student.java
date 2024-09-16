b66843b1a83b4230b1f17c6aa74276eae5713549
saval <saval@10.48.32.15>
2024-09-16 09:35:45 -0400

    getter and setter are added

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -20,5 +20,14 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+    
+    
+    public String getaddress() {
+        return name;
+    }
+
+    public void setaddress(String name) {
+        this.address = add;
+    }
    
 }
